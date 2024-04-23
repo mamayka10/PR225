d3559c17d5c4f2add9832eb198ba604f8f0dde06
Александр <160761957+mamayka10@users.noreply.github.com>
2024-04-23 20:02:45 +0300

    Delete rtr.py

--- rtr.py
+++
@@ -1,9 +0,0 @@
-import inspect
-import classes
-
-clss = inspect.getmembers(classes, inspect.isclass)
-
-for cls in clss:
-    methods = inspect.getmembers(clss., inspect.isfunction)
-members[0][1]()
-print(members)
