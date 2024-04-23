d4cae98b8918c686a90b6899bc6182a902267c89
Александр <160761957+mamayka10@users.noreply.github.com>
2024-04-23 20:00:11 +0300

    Add files via upload

---
+++ rtr.py
@@ -0,0 +1,9 @@
+import inspect
+import classes
+
+clss = inspect.getmembers(classes, inspect.isclass)
+
+for cls in clss:
+    methods = inspect.getmembers(clss., inspect.isfunction)
+members[0][1]()
+print(members)
---
+++ ttt.py
@@ -0,0 +1,12 @@
+class Person:
+
+    @staticmethod
+    def sayHi(): print("Hello Person")
+
+class Student():
+
+    @staticmethod
+    def sayHi(): print("Hello Student")
+
+def f1():
+    print("Hello F1")
